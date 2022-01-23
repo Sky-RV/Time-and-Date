b295d7020aeb9c277da1c5b999e8ee9f0b624f83
Sky-RV <SkyRivera89@gmail.com>
2022-01-23 22:30:40 +0330

    Body of main screen

--- Date.py
+++ Date.py
@@ -5,7 +5,24 @@ from os import system
 from colorama import init, Fore, Style
 from pyfiglet import Figlet
 
+################################################## BODY ##################################################
+
 system('cls') # clear the cmd screen
 
 banner = Figlet(font="banner3")
-print(Fore.GREEN + banner.renderText("Date Time") + Fore.WHITE)
+print(Fore.GREEN + banner.renderText(" Date Time") + Fore.WHITE)
+
+print("\n")
+
+print(" What do you want to use?")
+print(
+    "  1. Persian Monthly Calendar\n" +
+    "  2. English Monthly Calendar\n" +
+    "  3. Timer\n" +
+    "  4. Cornometer\n" +
+    "  5. Convert Persian Calendar to English\n" +
+    "  6. Convert English Calendar to Persian"
+)
+
+choose_input = input(Fore.BLUE + ">> " + Fore.WHITE)
+
