82e74f4c35efec8108f7cd5ef28005720add0f09
Sky-RV <SkyRivera89@gmail.com>
2022-01-23 22:38:32 +0330

    Main function and Modules

--- Date.py
+++ Date.py
@@ -5,24 +5,48 @@ from os import system
 from colorama import init, Fore, Style
 from pyfiglet import Figlet
 
+################################################## MAIN ##################################################
+
+def main():
+    print(Style.RESET_ALL)
+    
+    main_input = input(Fore.BLUE + ">> " + Fore.WHITE)
+
+    if main_input == "1":
+        pass
+    elif main_input == "2":
+        pass
+    elif main_input == "3":
+        pass
+    elif main_input == "4":
+        pass
+    elif main_input == "5":
+        pass
+    elif main_input == "6":
+        pass
+    else:
+        print(Fore.RED + ' Please enter a valid number...')
+        main()
+
 ################################################## BODY ##################################################
 
-system('cls') # clear the cmd screen
+def body():
+    system('cls') # clear the cmd screen
 
-banner = Figlet(font="banner3")
-print(Fore.GREEN + banner.renderText(" Date Time") + Fore.WHITE)
+    banner = Figlet(font="banner3")
+    print(Fore.GREEN + banner.renderText(" Date Time") + Fore.WHITE)
 
-print("\n")
+    print("\n")
 
-print(" What do you want to use?")
-print(
-    "  1. Persian Monthly Calendar\n" +
-    "  2. English Monthly Calendar\n" +
-    "  3. Timer\n" +
-    "  4. Cornometer\n" +
-    "  5. Convert Persian Calendar to English\n" +
-    "  6. Convert English Calendar to Persian"
-)
+    print(" What do you want to use?")
+    print(
+        "  1. Persian Monthly Calendar\n" +
+        "  2. English Monthly Calendar\n" +
+        "  3. Timer\n" +
+        "  4. Cornometer\n" +
+        "  5. Convert Persian Calendar to English\n" +
+        "  6. Convert English Calendar to Persian"
+    )
 
-choose_input = input(Fore.BLUE + ">> " + Fore.WHITE)
-
+body()
+main()
