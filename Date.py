0d4f9c2ab0f546c6293008a9895dc59f8ac8038d
Sky-RV <SkyRivera89@gmail.com>
2022-01-23 23:55:37 +0330

    English monthly Calendar

--- Date.py
+++ Date.py
@@ -4,6 +4,14 @@ import colorama
 from os import system
 from colorama import init, Fore, Style
 from pyfiglet import Figlet
+import time
+import calendar
+
+DAYS = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+DAYS_small = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+MONTHS_small = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', "Aug", 'Sep', 'Oct', 'Nov', 'Dec']
 
 ################################################## MAIN ##################################################
 
@@ -15,7 +23,7 @@ def main():
     if main_input == "1":
         pass
     elif main_input == "2":
-        pass
+        English_Monthly_Calendar()
     elif main_input == "3":
         pass
     elif main_input == "4":
@@ -28,6 +36,49 @@ def main():
         print(Fore.RED + ' Please enter a valid number...')
         main() # if you want in input more and more in one screen
         # body() # if you want to reset the code and enter new input in new screen
+
+################################################## PERSIAN MONTHLY CALENDAR ##################################################
+
+################################################## ENGLISH MONTHLY CALENDAR ##################################################
+
+def English_Monthly_Calendar():
+    system('cls')
+    print(Fore.YELLOW + " English Monthly Calendar\n\n")
+
+    print(Fore.WHITE + ' Enter Month (1-12) :')
+    en_month_input = input(Fore.BLUE + " >> ")
+
+    now = time.ctime()
+    this_year = time.strftime('%Y')
+    this_month_num = time.strftime("%m") # this month in number
+    this_month_voc = time.strftime("%B") # this month in words
+
+    print('\n')
+
+    print(Fore.CYAN + " " + calendar.month(int(this_year), int(en_month_input)))
+    print(Style.RESET_ALL)
+
+    print(Fore.WHITE + " Back to main ? (y/n)")
+    answer = input(Fore.BLUE + ' >> ')
+    if answer == 'y':
+        body()
+    else:
+        pass
+
+    # print(Fore.CYAN + "\t " + this_year + " " + this_month_voc)
+    # print(Fore.CYAN + " " + " ".join(DAYS_small))
+    # print(Style.RESET_ALL)
+
+    # for i in range(1, 13):
+        
+
+################################################## TIMER ##################################################
+
+################################################## CORNOMETER ##################################################
+
+################################################## CONVERT P to E CALENDAR ##################################################
+
+################################################## CONVERT E to P CALENDAR ##################################################
 
 ################################################## BODY ##################################################
 
@@ -48,6 +99,8 @@ def body():
         "  5. Convert Persian Calendar to English\n" +
         "  6. Convert English Calendar to Persian"
     )
+
+    main()
 
 body()
 main()
